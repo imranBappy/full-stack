9fbd858f61849bca69a334a3b31d2be772b5b4cb
imranbappy <imranhosenbappy690@gmail.com>
2021-07-08 11:51:17 +0600

    done

--- client/src/components/ScrollBar/ScrollBar.js
+++ client/src/components/ScrollBar/ScrollBar.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import './ScrollBar.css';
+import { useLocation } from 'react-router-dom';
 import {Link} from 'react-router-dom';
 const ScrollBar = (props) => {
+    const location = useLocation();
     return (
        <div className="container">
             <div className="scroll-bar">
             <ul>
                 {
-                    props.data.map((i, n)=><li key={i.path+n}>
-                            
+                    props.data.map((i, n)=>{
+                        const path = i.path.split('?')
+                        return(
+                            <li key={i.path+n}>
                                 <Link to={i.path}>
-                                <button>{i.name}</button>
+                                    <button
+                                        style={location.pathname === path[0] ? {background:'orangered'}: {background:'white'}}
+                                    >{i.name}</button>
                                 </Link>
-                        </li>
+                            </li>
+                        )
+                    }
                     )
                 }
             </ul>
